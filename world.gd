955d7c94a39a939666c28e4ea40b219a7af123c2
nikopeiris <78512931+nikopeiris@users.noreply.github.com>
2023-08-06 22:55:54 +1200

    Add files via upload

--- world.gd
+++ world.gd
@@ -1,7 +1,23 @@
 extends Node3D
 
 @onready var player = $Player
+@onready var parent = $"."
+var player_death = false
 
 #send players location to enemy
 func _physics_process(_delta):
-	get_tree().call_group("enemies", "update_location", player.global_transform.origin)
+	if player_death == false:
+		get_tree().call_group("enemies", "update_location", player.global_transform.origin)
+		
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_player_player_death():
+	player_death = true
+	var end_screen = preload("res://end_screen/end_screen.tscn").instantiate()
+	end_screen.text = "You Died"
+	get_tree().current_scene.add_child(end_screen)
+	#LoadingScreenCall.call_loading_screen("res://level_1/level_1.tscn", parent)
